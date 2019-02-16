b861d10b053cd95186b17ab2ba811f5788a5c64d
Francisco <francisco.pinchentti@gmail.com>
2019-02-16 09:05:10 -0300

    added an initial dataset: it will load on empty storage once

--- src/app/services/resume.service.ts
+++ src/app/services/resume.service.ts
@@ -2,10 +2,21 @@ import { Injectable } from '@angular/core';
 import { Resume } from '../models/Resume';
 import { Observable, of } from 'rxjs';
 
+const INITIAL_DATASET: Resume[] = [
+  { "id": "1", "fullname": "Jony Tolengo", "position": "DevOps", "bio": "I like to hammer stuff", "skills": [{ "name": "docker", "grade": 5 }, { "name": "bash", "grade": 3 }], "languages": [{ "name": "english", "grade": 2 }], "experiences": [{ "position": "devops", "company": "GloboSysCorp", "summary": "Server maintenance and I wrote many pipelines!", "from": "2015-02-10T03:00:00.000Z", "to": "2019-02-21T03:00:00.000Z" }], "contact": { "email": "a@a.com", "github": "jt.github.com", "linkedIn": "", "skypeID": "jt.sk.hammernow", "website": "www.thehammerjt.com" } },
+  { "id": "2", "fullname": "John Doe", "position": "Fullstack developer", "bio": "By day I code, at nights I fight crime with \"bash\"", "skills": [{ "name": "javascript", "grade": 4 }, { "name": "java", "grade": 4 }, { "name": "mysql", "grade": 3 }, { "name": "mongodb", "grade": 2 }, { "name": "git", "grade": 5 }, { "name": "bash", "grade": 5 }], "languages": [{ "name": "italian", "grade": 5 }, { "name": "english", "grade": 3 }], "experiences": [{ "position": "Backend developer", "company": "GL", "summary": "mantaned a java mvc application", "from": "2011-02-16T03:00:00.000Z", "to": "2014-02-10T03:00:00.000Z" }, { "position": "Frontend developer", "company": "InfoSys Supercorp", "summary": "We developed a mobile client for a secret project don't tell anyone", "from": "2014-03-01T03:00:00.000Z", "to": "2018-12-31T03:00:00.000Z" }], "contact": { "email": "jd@nomail.com", "github": "jd.nono.github.com", "linkedIn": "", "skypeID": "", "website": "www.jdnnn.com" } }
+];
+
 @Injectable()
 export class ResumeService {
 
-  constructor() { }
+  constructor() {
+    if (Object.keys(localStorage).length === 0) {
+      INITIAL_DATASET.forEach(o => {
+        localStorage.setItem(o.id, JSON.stringify(o));
+      });
+    }
+  }
 
   listResumes(): Observable<Resume[]> {
     const resumes = Object.keys(localStorage).map(k => {
